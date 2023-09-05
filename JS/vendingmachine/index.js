37e282ce79d1394626762ce6c587c73e90500f18
sejungseo <ssj1565@naver.com>
2023-09-05 23:47:40 +0900

    FES7

--- JS/vendingmachine/index.js
+++ JS/vendingmachine/index.js
@@ -1,4 +1,10 @@
+// 모듈단위로 쪼개진 자바스크립트 파일을 한 군데 모아놓은 곳 *모듈(자바스크립트를 쪼개놓은 것)
+
 import ColaGenerator from "./js/colaGenerator.js";
+import VendingMachineFunc from "./js/vendingMachineFunc.js";
 
 const colaGenerator = new ColaGenerator();
-colaGenerator.setup();
+await colaGenerator.setup();
+
+const vendingMachine = new VendingMachineFunc();
+vendingMachine.setup();
